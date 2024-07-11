379461b85fe8ee196075c143a7dc2428a2cbc355
MANEEESH ADITHYA <93572321+Maneesh-1212@users.noreply.github.com>
2024-07-11 19:47:36 +0530

    Add files via upload

---
+++ python numbers and casting.py
@@ -0,0 +1,37 @@
+# convert from one type to another
+
+x = 1    
+y = 2.8  
+z = 1j   
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# python casting
+# casting is a process of converting the value of one type into the value of another 
+
+x= int(1)
+y=float(5)
+z=complex(3)
+e=int(4.0)
+t=str(5)
+
+print(x)
+print(y)
+print(z)
+print(e)
+
