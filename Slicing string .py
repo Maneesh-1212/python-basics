69e5605e69413defe3f4a3bc6b44d1833ab0ad75
MANEEESH ADITHYA <93572321+Maneesh-1212@users.noreply.github.com>
2024-07-11 21:33:12 +0530

    Add files via upload

---
+++ Slicing string .py
@@ -0,0 +1,29 @@
+
+# You can return a range of characters by using the slice syntax.
+
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+x= "maneesh adithya"
+print(x[0])
+
+y="maneesh adithya"
+print(y[1])
+
+z="maneesh adithya"
+print(z[2:5])
+
+# slice from the start 
+# by leaving out the start index . the range will start at the first character 
+
+x= 'maneesh'
+print(x[:5])
+
+# slice to the end 
+x= "maneesh"
+print(x[2:])
+
+# negative indexing 
+
+z="maneesh adithya"
+print(z[-5:-2])
+
