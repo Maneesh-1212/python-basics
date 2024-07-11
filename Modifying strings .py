f8a1b742fa73b5c8ba8dd4c4d1b7f351cf606112
MANEEESH ADITHYA <93572321+Maneesh-1212@users.noreply.github.com>
2024-07-11 22:05:10 +0530

    Add files via upload

---
+++ Modifying strings .py
@@ -0,0 +1,21 @@
+# Upper case
+x= "hello, world"
+print(x.upper())
+
+# lowercase
+
+y='MANEESH'
+print(y.lower())
+
+# remove whitespace 
+a=" Hello world "
+print(a.strip())
+
+# replace string
+y= "adithya satya sai"
+print(y.replace("a","g"))
+
+# split string
+t="maneesh"
+print(a.split(","))
+
